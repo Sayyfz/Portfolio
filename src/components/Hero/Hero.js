35fe0b5b1728d827f1038057076b7e9e7d9ef53b
Seif Ashraf <seif_fire2@hotmail.com>
2022-09-14 18:11:18 +0200

    feat: hero done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui quo reiciendis dolorem cum eum vel minima nihil vitae, ullam accusamus repellendus et quisquam nemo delectus eligendi ducimus temporibus dolorum!
+      </SectionText>
+      <Button onClick={() => windows.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
